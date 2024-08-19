82fc6eeb2b5c05a1661166c2ad72876bc19f3508
CatsEleven <hatijolove@gmail.com>
2024-08-19 16:58:14 +0900

    add

---
+++ tests/test.py
@@ -0,0 +1,23 @@
+import unittest
+
+from speciallecture.CSVPrinter import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+    def test_read(self):
+        printer = CSVPrinter("./sample.csv")
+        line = printer.read()
+        print(line)
+        self.assertEqual(3, len(line))
+
+    def test_read2(self):
+        printer = CSVPrinter("./sample.csv")
+        line = printer.read()
+        self.assertEqual(3, len(line[1]))
+
+    def test_read3(self):
+        try:
+            printer = CSVPrinter("./sample.csv")
+            printer.read()
+            unittest.TestCase.fail("this line should not be involked")
+        except FileNotFoundError as e:
+            pass
